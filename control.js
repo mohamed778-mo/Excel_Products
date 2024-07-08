aff5195429416c2c0430f8c04e97328249a7359a
m.elmala <137796091+mohamed778-mo@users.noreply.github.com>
2024-07-08 04:30:00 +0300

    Update control.js

--- control.js
+++ control.js
@@ -4,7 +4,7 @@ const schema = require('./model')
 const get_products = (req, res) => {
     const lang = req.query.lang || 'EN';
   
-    readXlsxFile('./TEMP (1) (1).xlsx', { schema })
+    readXlsxFile('./TEMP (1).xlsx', { schema })
       .then(({ rows, errors }) => {
         if (errors.length === 0) {
           const filteredProducts = rows.map(row => ({
